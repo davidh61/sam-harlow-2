db365bda91e40c717399a488c6b6531c03226ed7
davidh61 <davidh_95@hotmail.com>
2023-04-04 00:05:03 +0100

    updated deployment path

--- portfolio/gridsome.config.js
+++ portfolio/gridsome.config.js
@@ -5,6 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Samuel harlow',
+  plugins: [],
+  siteUrl: 'https://davidh61.github.io',
+  pathPrefix: '/sam-harlow-2',
 }
