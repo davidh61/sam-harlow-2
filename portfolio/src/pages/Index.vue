00aca62ce43156b03f233ceb7b42ba402245f187
davidh61 <davidh_95@hotmail.com>
2023-04-05 16:28:59 +0100

    updated click handler

--- portfolio/src/pages/Index.vue
+++ portfolio/src/pages/Index.vue
@@ -4,7 +4,7 @@
     <body>
       <div class="header-container">
         <h1>Sam Harlow</h1>
-        <svg class="arrows" @click="scrollToPortfolio">
+        <svg class="arrows" v-on:click="scrollToPortfolio">
           <path class="a1" d="M0 0 L30 32 L60 0"></path>
           <path class="a2" d="M0 20 L30 52 L60 20"></path>
           <path class="a3" d="M0 40 L30 72 L60 40"></path>
